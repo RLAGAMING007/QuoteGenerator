e13277303c57f00823c9cb4fb6c229238ace60d0
RLAGAMING007 <k.l.kumar006@gmail.com>
2024-01-23 15:29:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import ReplayIcon from "@mui/icons-material/Replay";
+
+const App = () => {
+  let quotes = [];
+
+  async function loadeQuotes() {
+    const response = await fetch("https://type.fit/api/quotes");
+    quotes = await response.json();
+  }
+
+  const [quote, setQuote] = useState({
+    text: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    author: "Thomas Edison",
+  });
+  const random = () => {
+    const select = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(select);
+  };
+
+  loadeQuotes();
+
+  return (
+    <>
+      <div className="container">
+        <div className="quote">{quote.text}</div>
+        <div className="line"></div>
+        <div className="bottom">
+          <div className="author">{quote.author.split(",")[0]}</div>
+          <div className="relod__btn">
+            <button
+              onClick={() => {
+                random();
+              }}
+            >
+              <ReplayIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
