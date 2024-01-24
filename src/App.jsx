1377a00dc9562a7adb04ef96e2ace73cfc3ead56
RLAGAMING007 <k.l.kumar006@gmail.com>
2024-01-24 15:49:12 +0530

    quote generator using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import React, { useState } from "react";
-import ReplayIcon from "@mui/icons-material/Replay";
+import React from "react";
+import "./components/RandomQuotes.css";
+import RandomQuotes from "./components/RandomQuotes";
+import RandomQuotesAxios from "./components/RandomQuotesAxios";
 
-const App = () => {
-  let quotes = [];
-
-  async function loadeQuotes() {
-    const response = await fetch("https://type.fit/api/quotes");
-    quotes = await response.json();
-  }
-
-  const [quote, setQuote] = useState({
-    text: "Genius is one percent inspiration and ninety-nine percent perspiration.",
-    author: "Thomas Edison",
-  });
-  const random = () => {
-    const select = quotes[Math.floor(Math.random() * quotes.length)];
-    setQuote(select);
-  };
-
-  loadeQuotes();
-
+function App() {
   return (
     <>
-      <div className="container">
-        <div className="quote">{quote.text}</div>
-        <div className="line"></div>
-        <div className="bottom">
-          <div className="author">{quote.author.split(",")[0]}</div>
-          <div className="relod__btn">
-            <button
-              onClick={() => {
-                random();
-              }}
-            >
-              <ReplayIcon />
-            </button>
-          </div>
-        </div>
-      </div>
+      <RandomQuotes />
+      <RandomQuotesAxios />
     </>
   );
-};
+}
 
 export default App;
